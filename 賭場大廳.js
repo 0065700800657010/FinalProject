3828cdf718a1e03f419a554323a78cacf371ed3d
0065700800657010 <00657008.10.webdesign@gmail.com>
2019-01-07 14:45:13 +0800

    GroTion-test1
    GroTion-test1

---
+++ 賭場大廳.js
@@ -0,0 +1,9 @@
+// JavaScript source code
+function start()
+{
+    //var name=window.prompt("Please enter your name.");
+    document.getElementById("Hello").innerHTML+="Hello,"+name+":";
+
+}
+
+window.addEventListener("load",start,false);
